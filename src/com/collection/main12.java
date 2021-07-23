3ec645d9e1e13b3c244195800e8dee03221afaa1
wahidullahmayar2020 <wahidullahmayar2020@gmail.com>
2021-07-23 18:20:20 -0400

    3 COMMIT

--- src/com/collection/main12.java
+++ src/com/collection/main12.java
@@ -2,7 +2,7 @@ package com.collection;
 
 public class main12 {
     public static void main(String[] args) throws InterruptedException {
-        for (int i = 0; i <5 ; i++) {
+        for (int i = 0; i <6 ; i++) {
             System.out.println("Batch 9 us great ");
             Thread.sleep(1000);
         }
