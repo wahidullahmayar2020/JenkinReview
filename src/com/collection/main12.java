72054276f7098fbcecaa33ea8b6655d17c1238aa
wahidullahmayar2020 <wahidullahmayar2020@gmail.com>
2021-07-23 17:51:51 -0400

    First Commit updated

---
+++ src/com/collection/main12.java
@@ -0,0 +1,10 @@
+package com.collection;
+
+public class main12 {
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i <5 ; i++) {
+            System.out.println("Batch 9 us great ");
+            Thread.sleep(1000);
+        }
+    }
+}
